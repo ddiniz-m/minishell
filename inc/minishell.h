0cb645148d6056e9e44769379d3140fd24760953
WudDoo <89762267+WudDoo@users.noreply.github.com>
2023-06-16 17:03:13 +0100

    fixed error: ^C would exit the shell when used on the first prompt

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,25 +6,30 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:01:34 by ddiniz-m          #+#    #+#             */
-/*   Updated: 2023/06/13 18:27:10 by mortins-         ###   ########.fr       */
+/*   Updated: 2023/06/16 17:00:20 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-//--------------------------------INCLUDES-------------------------------------
-#include "../libft/src/libft.h"
+//----------------------------------INCLUDES-----------------------------------
+# include "../libft/src/libft.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 //readline, rl_on_new_line, rl_replace_line, rl_redisplay
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 //signal
-#include <signal.h>
+# include <signal.h>
+
+//-----------------------------------SIGNALS-----------------------------------
+void	signal_init(void);
+void	signal_interrupt(int signum);
+void	signal_exit(char *str);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,53 +6,24 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:59:22 by ddiniz-m          #+#    #+#             */
-/*   Updated: 2023/06/16 13:42:39 by mortins-         ###   ########.fr       */
+/*   Updated: 2023/06/16 16:59:56 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-//CTRL-C sends a "Interrupt" signal.
-//Don't want to explain rl functions, go read:
-//https://tiswww.case.edu/php/chet/readline/readline.html#Command-Line-Editing
-void	signal_interrupt(int signum)
-{
-	if (signum == SIGINT)
-	{
-		printf("\n");
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-	}	
-}
-
-//CTRL-D represents "No input", so when it is pressed, "str" is NULL and it
-//	exits.
-//CTRL-\ sends a "Quit" signal. When using SIG_IGN in signal(), it ignores 
-//	the signal recieved.
-void	signal_init(char *str)
-{
-	signal(SIGINT, signal_interrupt);
-	signal(SIGQUIT, SIG_IGN);
-	if (!str)
-	{
-		printf("exit\n");
-		free(str);
-		exit (0);
-	}
-}
-
-//	readline records whatever is inputed in terminal, and returns a memory
+//	'readline' records whatever is inputed in terminal, and returns a memory
 //	allocated char *buffer
 int	main(void)
 {
 	char	*str;
 
+	signal_init();
 	while (1)
 	{
 		str = readline("Minishell >$ ");
 		add_history(str);
-		signal_init(str);
+		signal_exit(str);
 		free(str);
 	}
 }
