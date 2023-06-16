5427a249439410b3db4b2d8cabe6e37f10fc2a89
ddiniz-m <diogodinizm@gmail.com>
2023-06-16 19:10:36 +0100

    test file with some functions

---
+++ test.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddiniz-m <ddiniz-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 12:53:10 by ddiniz-m          #+#    #+#             */
+/*   Updated: 2023/06/16 12:53:20 by ddiniz-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* void	get_path_from_env(void)
+{
+	char *str;
+
+	str = getenv("PATH");
+	printf("\nPATH = %s\n", str);
+} */
+
+/* void	get_files_dirs(void)
+{
+	DIR				*folder;
+	struct dirent	*buf;
+
+	printf("\nFiles and Directories:\n");
+	folder = opendir(".");
+	while ((buf = readdir(folder)))
+		printf("%s\n", buf->d_name);
+	closedir(folder);
+} */
+
+/* void	get_directory(void)
+{
+	printf("Current Directory = %s\n", getcwd(NULL, 0));
+} */
