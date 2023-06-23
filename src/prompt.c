32148b158c8d61ae458e79de749310eb9068bbaf
WudDoo <89762267+WudDoo@users.noreply.github.com>
2023-06-23 17:18:21 +0100

    malloc error protection and norme compliance

--- src/prompt.c
+++ src/prompt.c
@@ -6,34 +6,33 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 18:40:31 by mortins-          #+#    #+#             */
-/*   Updated: 2023/06/23 16:29:16 by mortins-         ###   ########.fr       */
+/*   Updated: 2023/06/23 17:16:05 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-char	*set_prompt(void)
+char	*set_prompt(t_var *var)
 {
 	char	cwd[PATH_MAX];
 	char	*prompt;
 	char	*buf;
-	char	*buf2;
 
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 	{
 		prompt = malloc(sizeof(char) * 25);
 		if (!prompt)
-			exit (1);
+			malloc_error(var);
 		prompt = "\033[1;33mMinishell $> \033[0m";
 		return (prompt);
 	}
-	buf = malloc(sizeof(char) * ft_strlen(ft_strrchr(getcwd(cwd, sizeof(cwd)), '/')));
+	buf = ft_strjoin("\033[1;33m", ft_strrchr(getcwd(cwd, sizeof(cwd)), '/') \
+		+ 1);
 	if (!buf)
-		exit (1);
-	ft_strcpy(buf, ft_strrchr(getcwd(cwd, sizeof(cwd)), '/') + 1);
-	buf2 = ft_strjoin("\033[1;33m", buf);
+		malloc_error(var);
+	prompt = ft_strjoin(buf, " $> \033[0m");
+	if (!prompt)
+		malloc_error(var);
 	free(buf);
-	prompt = ft_strjoin(buf2, " $> \033[0m");
-	free(buf2);
 	return (prompt);
 }
